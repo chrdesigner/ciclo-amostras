67d3e3a154261a63ba56351de824be7919d462ab
Cesar Ribeiro <chrdesigner@server.chrdesigner.private>
2016-05-16 14:43:23 -0300

    Fixed bug - Login

--- templates/header-restitra.php
+++ templates/header-restitra.php
@@ -11,7 +11,20 @@
 		<nav>
 			<ul>
 				<li>Meu Perfil</li>
-				<?php $args = [ 'post_type' => 'page', 'posts_per_page' => 1, 'fields' => 'ids', 'nopaging' => true, 'meta_key' => '_wp_page_template', 'meta_value' => '../template-restrita.php' ]; $pages = get_posts( $args ); foreach ( $pages as $page ) { ?>
+				<?php
+					
+					$args = array (
+						'post_type' => 'page',
+						'posts_per_page' => 1,
+						'fields' => 'ids',
+						'nopaging' => true,
+						'meta_key' => '_wp_page_template',
+						'meta_value' => '../template-restrita.php'
+					);
+					
+					$pages = get_posts( $args ); foreach ( $pages as $page ) { 
+
+				?>
 				<li><a href="<?php echo esc_url( get_permalink($page) ); ?>" title="Informações Iniciais." class="dashicons-before dashicons-admin-home"></a></li><?php }; ?>
 				<?php $args = array ( 'post_type' => array( 'promotor' ), 'post_status' => array( 'publish' ), 'author' => get_current_user_id(), 'posts_per_page' => 1, ); $loop_promotor = new WP_Query( $args ); if ( $loop_promotor->have_posts() ) { while ( $loop_promotor->have_posts() ) { $loop_promotor->the_post(); ?>
 				<li><a href="<?php the_permalink();?>" title="Meu Cadastro." class="dashicons-before dashicons-admin-generic"></a></li><?php } } wp_reset_postdata(); ?>
